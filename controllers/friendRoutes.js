a7a6fcec491987bf8f26e88fd330b7064f916d32
tommy8969 <tommy8969@naver.com>
2023-09-23 17:12:24 +0900

    [in progress] - 사용자 토큰 검증

--- controllers/friendRoutes.js
+++ controllers/friendRoutes.js
@@ -1,23 +1,41 @@
 const bodyParser = require('body-parser');
+const Account = require('../model/Account');
+const options = require('../config/secretkey');
+const jwt = require('jsonwebtoken')
 
-const userSchema = new mongoose.Schema({
+/* const userSchema = new mongoose.Schema({
     username: String,
     friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  });
+  }); */
   
-  const User = mongoose.model('User', userSchema);
-  
-  //친구 추가
+
+  const User = mongoose.model('User', accountSchema); 
+
+  // 토큰 검증 미들웨어
+  function autenticateToken(req, res, next) {
+
+    const token = req.headers.authorization;
+
+    jwt.verify(token, options.secretKey, (err, user) => {
+      if (err){
+        return res.status(400).json({message : '토큰 검증 실패'});
+      }
+    
+      req.user = user;
+      next();
+
+    })
+  }
+    
+  // 친구 추가
   app.post('/sendfriendrequest', async (req, res) => {
     try {
-      const { userId, friendId } = req.body;
+      const userId = req.user.email;
+      const friendId = req.body;
   
       const user = await User.findOne({ _id: userId });
       const friend = await User.findOne({ _id: friendId });
   
-      user.friends.push(friendId);
-      friend.friends.push(userId);
-
     // 친구 요청 목록에 추가
     user.friendRequests.push(friendId);
     await user.save();
@@ -32,7 +50,8 @@ const userSchema = new mongoose.Schema({
 // 친구 요청 수락
 app.post('/acceptfriendrequest', async (req, res) => {
   try {
-    const { userId, friendId } = req.body;
+    const userId = req.user.email;
+    const friendId = req.body;
 
     const user = await User.findOne({ _id: userId });
     const friend = await User.findOne({ _id: friendId });
@@ -56,7 +75,8 @@ app.post('/acceptfriendrequest', async (req, res) => {
 // 친구 요청 거절
 app.post('/declinefriendrequest', async (req, res) => {
   try {
-    const { userId, friendId } = req.body;
+    const userId = req.user.email;
+    const friendId = req.body;
 
     const user = await User.findOne({ _id: userId });
     const friend = await User.findOne({ _id: friendId });
@@ -75,12 +95,12 @@ app.post('/declinefriendrequest', async (req, res) => {
   }
 });
 
-  //친구 목록
+// 친구 목록
 app.get('/friendlist', async (req, res) => {
   try {
       const userId = req.params.userId;
 
-      const user = await User.findById(userId).populate('friends');
+      const user = await User.findOne({ _id: userId }).populate('friends');
 
       res.status(200).json({ friends: user.friends });
   } catch (error) {
@@ -89,10 +109,11 @@ app.get('/friendlist', async (req, res) => {
   }
 });
 
-//친구 삭제
+// 친구 삭제
 app.delete('/removefriend', async (req, res) => {
   try {
-      const { userId, friendId } = req.body;
+      const userId = req.user.email;
+      const friendId = req.body;
 
       const user = await User.findOne({ _id: userId });
       const friend = await User.findOne({ _id: friendId });
